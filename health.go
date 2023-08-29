70ffa2d5c4e5b541c4ac940b1242d7e4c71b82d4
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2023-08-29 17:38:31 +0100

    modify health to passed the test

--- health.go
+++ health.go
@@ -4,14 +4,12 @@ import "net/http"
 
 func (s *Server) Health() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			s.logger.Info("Status Ok", "name", "get health")
-			w.WriteHeader(http.StatusOK)
-			return
-		} else {
+		if r.Method != http.MethodGet {
 			s.logger.Info("Bad request", "name", "get health")
-			w.WriteHeader(http.StatusBadRequest)
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			return
 		}
+		s.logger.Info("Status Ok", "name", "get health")
+		w.WriteHeader(http.StatusOK)
 	}
 }
